1e40c6fa48ef1aca969d0b8793597cb13d510b17
Anuj Srivastava <107947346+StonnedSanta@users.noreply.github.com>
2023-03-13 21:04:54 +0530

    Add files via upload
    Work in progress...

---
+++ telecom_customer_churn.sql
@@ -0,0 +1,10 @@
+use telecom_churndb;
+
+select * 
+from telecom_customer_churn;
+
+-- How many customers joined the company during the last quarter? How many customers joined?
+select tenure, count(id)
+from telecom_customer_churn
+group by tenure
+having tenure <=3
